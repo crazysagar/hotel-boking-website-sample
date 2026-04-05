40121130973c292a4e6ed3bc229421b82611b132
Sagar Shrestha <crazysagar9876543210@gmail.com>
2026-04-05 14:18:06 +0545

    facilities page created

---
+++ facilities.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>EoS Hotel-Facilities</title>
+    <?php require('inc/links.php') ?>
+<style>
+  .pop:hover{
+  border-top-color: var(--teal) !important;
+  transform: scale(1.03);
+  transition: all 0.3s;
+  }
+</style>
+</head>
+
+<body class="bg-light">
+
+  <?php require('inc/header.php') ?>
+ 
+<div class="my-5 px-4">
+<h2 class="h-font fw-bokd text-center">OUR FACILITIES</h2>
+<div class="h-line bg-dark"></div>
+
+<p class="text-center mt-3">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+   Incidunt totam veniam magni, adipisci quam quo
+  </p>
+
+</div>
+<div class="container">
+  <div class="row">
+    <div class="col-lg-4 col-md-6 mb-5 px-4">
+      <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+        <div class="d-flex align-item-center mb-2">
+
+          <img src="https://cdn-icons-png.flaticon.com/512/93/93158.png" width="40px">
+          <h5 class="m-0 ms-3">Wifi</h5>
+
+        </div>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam ducimus atque facilis eius neque voluptatum provident nemo omnis tenetur nobis.</p>
+
+      </div>
+    </div>
+    <div class="col-lg-4 col-md-6 mb-5 px-4">
+      <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+        <div class="d-flex align-item-center mb-2">
+
+          <img src="https://cdn-icons-png.flaticon.com/512/93/93158.png" width="40px">
+          <h5 class="m-0 ms-3">Wifi</h5>
+
+        </div>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam ducimus atque facilis eius neque voluptatum provident nemo omnis tenetur nobis.</p>
+
+      </div>
+    </div>
+    <div class="col-lg-4 col-md-6 mb-5 px-4">
+      <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+        <div class="d-flex align-item-center mb-2">
+
+          <img src="https://cdn-icons-png.flaticon.com/512/93/93158.png" width="40px">
+          <h5 class="m-0 ms-3">Wifi</h5>
+
+        </div>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam ducimus atque facilis eius neque voluptatum provident nemo omnis tenetur nobis.</p>
+
+      </div>
+    </div>
+    <div class="col-lg-4 col-md-6 mb-5 px-4">
+      <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+        <div class="d-flex align-item-center mb-2">
+
+          <img src="https://cdn-icons-png.flaticon.com/512/93/93158.png" width="40px">
+          <h5 class="m-0 ms-3">Wifi</h5>
+
+        </div>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam ducimus atque facilis eius neque voluptatum provident nemo omnis tenetur nobis.</p>
+
+      </div>
+    </div>
+    <div class="col-lg-4 col-md-6 mb-5 px-4">
+      <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+        <div class="d-flex align-item-center mb-2">
+
+          <img src="https://cdn-icons-png.flaticon.com/512/93/93158.png" width="40px">
+          <h5 class="m-0 ms-3">Wifi</h5>
+
+        </div>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam ducimus atque facilis eius neque voluptatum provident nemo omnis tenetur nobis.</p>
+
+      </div>
+    </div>
+    <div class="col-lg-4 col-md-6 mb-5 px-4">
+      <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+        <div class="d-flex align-item-center mb-2">
+
+          <img src="https://cdn-icons-png.flaticon.com/512/93/93158.png" width="40px">
+          <h5 class="m-0 ms-3">Wifi</h5>
+
+        </div>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam ducimus atque facilis eius neque voluptatum provident nemo omnis tenetur nobis.</p>
+
+      </div>
+    </div>
+  </div>
+</div>
+  <?php require('inc/footer.php') ?>
+</body>
+
+</html>
