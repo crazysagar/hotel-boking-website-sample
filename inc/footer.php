84a434df99f7d6f8bea3c7d37a89a536557f135a
Sagar Shrestha <crazysagar9876543210@gmail.com>
2026-04-05 18:20:32 +0545

    created seperate file for common

---
+++ inc/footer.php
@@ -0,0 +1,32 @@
+ <div class="container-fluid bg-white mt-5">
+     <div class="row">
+         <div class="col-lg-4">
+
+             <h3 class="h-font fw-bold fs-3 mb-2">EoS Hotel</h3>
+             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+             </p>
+         </div>
+         <div class="col-lg-4 p-4">
+             <h5 class="mb-3">Links</h5>
+             <a href="#" class="d-inline-block mb-2 text-decoration-none text-dark">Home</a><br>
+             <a href="#" class="d-inline-block mb-2 text-decoration-none text-dark">Rooms</a><br>
+             <a href="#" class="d-inline-block mb-2 text-decoration-none text-dark">Facilities</a><br>
+             <a href="#" class="d-inline-block mb-2 text-decoration-none text-dark">About Us</a><br>
+             <a href="#" class="d-inline-block mb-2 text-decoration-none text-dark">Contact Us</a><br>
+         </div>
+
+         <div class="col-lg-4 p-4">
+             <h5 class="mb-3">Follow Us</h5>
+             <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none"><i class="bi bi-instagram me-1"></i> Instagram
+             </a><br>
+             <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none"><i class="bi bi-facebook me-1"></i> Facebook
+             </a><br>
+             <a href="#" class="d-inline-block text-dark text-decoration-none"><i class="bi bi-twitter me-1"></i> Twitter
+             </a>
+         </div>
+     </div>
+ </div>
+
+ <h6 class="text-center bg-dark text-white p-3 m-0">Designed and Developed By EoS WebDev</h6>
+
+ <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
