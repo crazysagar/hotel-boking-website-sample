2efbed2b07393a4d55885e7a31660d3e0d6b8b7a
Sagar Shrestha <crazysagar9876543210@gmail.com>
2026-04-08 18:07:42 +0545

    created all for one file

---
+++ admin/inc/links.php
@@ -0,0 +1,5 @@
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Merienda:wght@400;700&display=swap">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
+<link rel="stylesheet" href="css/common.css">
