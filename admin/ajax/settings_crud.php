a218027cbd5a97c93f60f204bde0898df796fae2
Sagar Shrestha <crazysagar9876543210@gmail.com>
2026-04-24 18:37:49 +0545

    created new post request

--- admin/ajax/settings_crud.php
+++ admin/ajax/settings_crud.php
@@ -32,13 +32,12 @@ if (isset($_POST['get_contacts']) && $_POST['get_contacts'] == 1) {
     $res = select($q, $values, "i");
     $data = mysqli_fetch_assoc($res);
     echo json_encode($data);
+    exit;
 }
-
-
 
 if (isset($_POST['upd_contacts'])) {
     $frm_data = filteration($_POST);
-    $q = "UPDATE `contact_details` SET `address`=?, `gmap`=?, `pn1`=?, `pn2`=?, `email`=?, `fb`=?, `insta`=?, `tw`=?, `iframe`=? WHERE `sno`=?";
+    $q = "UPDATE `contact_details` SET `address`=?, `gmap`=?, `pn1`=?, `pn2`=?, `email`=?, `fb`=?, `ig`=?, `tw`=?, `iframe`=? WHERE `sno`=?";
     $values = [
         $frm_data['address'],
         $frm_data['gmap'],
@@ -46,12 +45,34 @@ if (isset($_POST['upd_contacts'])) {
         $frm_data['pn2'],
         $frm_data['email'],
         $frm_data['fb'],
-        $frm_data['insta'],
+        $frm_data['ig'],
         $frm_data['tw'],
         $frm_data['iframe'],
         1
     ];
     $res = update($q, $values, 'sssssssssi');
     echo $res;
+}
+
+if (isset($_POST['add_member'])) {
+    $frm_data = filteration($_POST);
+
+    $img_r = uploadImage($_FILES['picture'], ABOUT_FOLDER);
+
+    if($img_r == 'inv_img'){
+        echo $img_r;
+
+    }else if($img_r == 'inv_size'){
+        echo $img_r;
+
+    }else if($img_r == 'upd_failed'){
+        echo $img_r;
+
+    } else {
+        $q = "INSERT INTO `team_details`(`name`, `picture`) VALUES (?,?)";
+        $values = [$frm_data['name'],$img_r];
+        $res = insert($q,$values,'ss');
+        echo $res;
+    }
 }
 ?>
