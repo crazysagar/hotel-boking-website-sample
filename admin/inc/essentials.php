11fa0fb303822955457fb28ad888113228330284
Sagar Shrestha <crazysagar9876543210@gmail.com>
2026-04-24 18:36:57 +0545

    added img upload function

--- admin/inc/essentials.php
+++ admin/inc/essentials.php
@@ -1,16 +1,21 @@
 <?php
 
-function adminLogin(){
+define('UPLOAD_IMAGE_PATH', $_SERVER['DOCUMENT_ROOT'] . '/hbwebsite/images/');
+define('ABOUT_FOLDER', 'about/');
+
+function adminLogin()
+{
     session_start();
-    if(!(isset($_SESSION['adminLogin']) && $_SESSION['adminLogin']==true)){
-header("location: index.php");
-    exit;
+    if (!(isset($_SESSION['adminLogin']) && $_SESSION['adminLogin'] == true)) {
+        header("location: index.php");
+        exit;
     }
     // session_regenerate_id(true);
 
 }
-function redirect($url) {
-    echo"
+function redirect($url)
+{
+    echo "
     <script>window.location.href='$url';
     </script>
     ";
@@ -27,4 +32,28 @@ function alert($type, $msg)
                  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
              </div>
        alert;
+}
+
+
+function uploadImage($image, $folder)
+{
+    $valid_mime = ['image/jpg', 'image/jpeg', 'image/png', 'image/webp'];
+    $img_mime = $image['type'];
+
+    // if(in_arary($img_mine,$valid_mime))
+    if (!in_array($img_mime, $valid_mime)) {
+        return 'inv_img'; //  invalid image mime or format
+    } else if (($image['size'] / (1024 * 1024)) > 2) {
+        return 'inv_size'; //invalid size greater than 2mb
+    } else {
+        $ext = pathinfo($image['name'], PATHINFO_EXTENSION);
+        $rname = 'IMG_' . random_int(11111, 99999) . ".$ext";
+        $img_path = UPLOAD_IMAGE_PATH . $folder . $rname;
+
+        if (move_uploaded_file($image['tmp_name'], $img_path)) {
+            return $rname;
+        } else {
+            return 'upd_failed';
+        }
+    }
 }
